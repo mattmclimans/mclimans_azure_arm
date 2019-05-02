d0ac02bce6e1406083d8ef8f94212e8fa0d38bf1
Matt McLimans <32170231+mattmclimans@users.noreply.github.com>
2019-05-01 21:32:16 -0400

    Update dbserver-startup.sh

--- scripts/dbserver-startup.sh
+++ scripts/dbserver-startup.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-FW_NIC3="10.5.2.4"
+FW_NIC0="10.5.0.4"
 while true
     do 
-        resp=$(curl -s -S -g -k "https://$FW_NIC3/api/?type=op&cmd=<show><chassis-ready></chassis-ready></show>&key=LUFRPT1CU0dMRHIrOWFET0JUNzNaTmRoYmkwdjBkWWM9alUvUjBFTTNEQm93Vmx0OVhFRlNkOXdJNmVwYWk5Zmw4bEs3NjgwMkh5QT0=")
+        resp=$(curl -s -S -g -k "https://$FW_NIC0/api/?type=op&cmd=<show><chassis-ready></chassis-ready></show>&key=LUFRPT1CU0dMRHIrOWFET0JUNzNaTmRoYmkwdjBkWWM9alUvUjBFTTNEQm93Vmx0OVhFRlNkOXdJNmVwYWk5Zmw4bEs3NjgwMkh5QT0=")
 	echo $resp
         if [[ $resp == *"[CDATA[yes"* ]] ; then
             break
